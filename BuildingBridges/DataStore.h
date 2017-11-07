3c138a79d0c315b0b0346edeae36f58a50ff2702
Tim Ekl <lithium3141@gmail.com>
2017-11-07 12:56:47 -0800

    Add an empty ObjC DataStore file
    Let Xcode configure the bridging header as usual
    
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

---
+++ BuildingBridges/DataStore.h
@@ -0,0 +1,13 @@
+//
+//  DataStore.h
+//  BuildingBridges
+//
+//  Created by Tim Ekl on 11/7/17.
+//  Copyright © 2017 Tim Ekl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataStore : NSObject
+
+@end
---
+++ BuildingBridges/DataStore.m
@@ -0,0 +1,13 @@
+//
+//  DataStore.m
+//  BuildingBridges
+//
+//  Created by Tim Ekl on 11/7/17.
+//  Copyright © 2017 Tim Ekl. All rights reserved.
+//
+
+#import "DataStore.h"
+
+@implementation DataStore
+
+@end
